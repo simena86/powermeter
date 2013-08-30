a5ecef69cf4aa126cbea56a0f045460ec618b520
simena86 <simena86@gmail.com>
2013-08-30 10:48:49 +0200

    added powerLogger

---
+++ src/logger/powerLogger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+"""
+Example of how to use GPIO and TCP interrupts with RPIO.
+RPIO Documentation: http://pythonhosted.org/RPIO
+"""
+import RPIO
+
+
+def gpio_callback(gpio_id, val):
+    print("gpio %s: %s" % (gpio_id, val))
+
+
+# Two GPIO interrupt callbacks (second one with a debouce timeout of 100ms)
+RPIO.add_interrupt_callback(14, gpio_callback, edge='rising', \
+        debounce_timeout_ms=100)
+
+# Starts waiting for interrupts (exit with Ctrl+C)
+RPIO.wait_for_interrupts()
+
+
