7cd6f9fab6810f36e613564af84482d4dc716af3
simena86 <simena86@gmail.com>
2013-10-03 23:38:23 +0000

    asdf

--- src/logger/updatePowerLog.py
+++ src/logger/updatePowerLog.py
@@ -3,21 +3,31 @@ import time
 from datetime import datetime
 
 def updateLog():
-	tmpFileName='/home/pi/powermeter/src/logger/powerTemp'
-	f=open(tmpFileName,'r')
-	nr=f.readline()
-	f.close()
-	f=open(tmpFileName,'w')
-	f.write('0')
-	f.close()
-
-	
-	fileName='/home/pi/powermeter/src/logger/power_data.csv'
-	f=open(fileName,'a')	
-	t=datetime.now()		
-	outStr= '%s,%s,%s,%s,%s,%s,%s' % (t.year, t.month, t.day, t.hour,t.minute, t.second,nr) 
-	f.write(outStr + '\n')
-	print 'impulses: ' + str(nr)
-	f.close()
+	try:
+		tmpFileName='/home/pi/powermeter/src/logger/powerTemp'
+		f=open(tmpFileName,'r')
+		nr=f.readline()
+		f.close()
+		f=open(tmpFileName,'w')
+		f.write('0')
+		f.close()
+	except Exception,e:
+		print e
+		f.write('0')
+		f.close()
+	try:
+		nr
+	except:
+		nr=0
+	try:
+		fileName='/home/pi/powermeter/src/logger/power_data.csv'
+		f=open(fileName,'a')	
+		t=datetime.now()		
+		outStr= '%s,%s,%s,%s,%s,%s,%s' % (t.year, t.month, t.day, t.hour,t.minute, t.second,nr) 
+		f.write(outStr + '\n')
+		f.close()
+	except Exception, e:
+		print e
+		f.close()
 
 updateLog()
